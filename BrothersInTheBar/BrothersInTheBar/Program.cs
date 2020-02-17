a8c7548c0fa93512496bc1432020500a1f0a8f29
KrumK <kkereziev@abv.bg>
2020-02-17 20:03:29 +0200

    Task one done.

--- BrothersInTheBar/BrothersInTheBar/Program.cs
+++ BrothersInTheBar/BrothersInTheBar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace BrothersInTheBar
@@ -13,6 +14,17 @@ namespace BrothersInTheBar
 
         public static int[] CreateArray()
         {
+            #region AlternativeCreation
+            //int arrayLength = int.Parse(Console.ReadLine());
+            //int[] array = new int[arrayLength];
+            //for (int i = 0; i < arrayLength; i++)
+            //{
+            //    int element = int.Parse(Console.ReadLine());
+            //    array[i] = element;
+            //}
+            //return array;
+            #endregion
+
             int[] array = Console.ReadLine()
                 .Split(", ")
                 .Select(int.Parse)
@@ -23,11 +35,11 @@ namespace BrothersInTheBar
         public static int FindRounds()
         {
             int[] array = CreateArray();
-            int rounds = 0;
+            var rounds = 0;
             var list=new List<int>(array);
             for (int i = 0; i < list.Count-2; i++)
             {
-                if (list[i]== list[i+1] && list[i]== list[i+2])
+                if (list[i] == list[i+1] && list[i] == list[i+2])
                 {
                     rounds++;
                     list.RemoveRange(i,3);
