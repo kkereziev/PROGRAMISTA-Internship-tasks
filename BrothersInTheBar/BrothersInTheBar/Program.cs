cd8504c1253d61306ad217be0856bd22280b13d2
KrumK <kkereziev@abv.bg>
2020-02-17 19:14:52 +0200

    Task one ready.

---
+++ BrothersInTheBar/BrothersInTheBar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrothersInTheBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"brothersInTheBar(glasses) = {FindRounds()}");
+        }
+
+        public static int[] CreateArray()
+        {
+            int[] array = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            return array;
+        }
+
+        public static int FindRounds()
+        {
+            int[] array = CreateArray();
+            int rounds = 0;
+            var list=new List<int>(array);
+            for (int i = 0; i < list.Count-2; i++)
+            {
+                if (list[i]== list[i+1] && list[i]== list[i+2])
+                {
+                    rounds++;
+                    list.RemoveRange(i,3);
+                    i=0;
+                }
+            }
+            return rounds;
+        }
+    }
+}
